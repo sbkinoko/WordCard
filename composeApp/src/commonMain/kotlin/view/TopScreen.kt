db3c39a81f912af34ff690fc275dcaa08a1a131d
sbknk <136720653+sbkinoko@users.noreply.github.com>
2024-10-16 22:55:52 +0900

    ボタン追加

--- composeApp/src/commonMain/kotlin/view/TopScreen.kt
+++ composeApp/src/commonMain/kotlin/view/TopScreen.kt
@@ -5,12 +5,17 @@ import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Button
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.unit.dp
+import io.realm.kotlin.internal.interop.ArrayAccessor
 import org.koin.compose.koinInject
 import viewmodel.TopViewModel
 
@@ -32,11 +37,26 @@ fun TopScreen(
             .padding(10.dp),
         verticalArrangement = Arrangement.spacedBy(5.dp),
     ) {
-        TopComponent(
-            text = "Hello, World!",
-            onClickDetail = {
-                topViewModel.onClick("Hello world")
+        LazyColumn(
+            modifier = Modifier.weight(1f),
+            verticalArrangement = Arrangement.spacedBy(5.dp),
+        ) {
+            item {
+                TopComponent(
+                    text = "Hello, World!",
+                    onClickDetail = {
+                        topViewModel.onClick("Hello world")
+                    }
+                )
             }
-        )
+        }
+        Button(
+            modifier = Modifier.fillMaxWidth(),
+            onClick = {
+
+            }
+        ) {
+            Text("+")
+        }
     }
 }
