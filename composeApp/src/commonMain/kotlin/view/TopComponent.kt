fd993339d954c4ad14b4665f9433cfa0150b39dc
sbknk <136720653+sbkinoko@users.noreply.github.com>
2024-10-18 21:44:54 +0900

    レイアウト修正

--- composeApp/src/commonMain/kotlin/view/TopComponent.kt
+++ composeApp/src/commonMain/kotlin/view/TopComponent.kt
@@ -1,12 +1,17 @@
 package view
 
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -19,13 +24,20 @@ fun TopComponent(
     modifier: Modifier = Modifier,
 ) {
     Row(
-        modifier = modifier,
+        modifier = modifier
+            .border(
+                width = 1.dp,
+                color = Color.Black,
+                shape = RoundedCornerShape(5.dp)
+            )
+            .padding(5.dp),
         horizontalArrangement = Arrangement.spacedBy(5.dp),
+        verticalAlignment = Alignment.CenterVertically,
     ) {
         TextField(
+            modifier = Modifier.weight(1f),
             value = text,
             onValueChange = onEditText,
-            maxLines = 1,
         )
         Button(
             onClick = onClickDetail
