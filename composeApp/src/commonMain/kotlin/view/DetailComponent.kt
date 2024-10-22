d8a0f592e8d3dd7d314859bc694afafeb03eaa5b
sbknk <136720653+sbkinoko@users.noreply.github.com>
2024-10-23 00:14:00 +0900

    アノテーション削除

--- composeApp/src/commonMain/kotlin/view/DetailComponent.kt
+++ composeApp/src/commonMain/kotlin/view/DetailComponent.kt
@@ -18,7 +18,6 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
@@ -28,7 +27,6 @@ import androidx.compose.ui.input.key.onKeyEvent
 import androidx.compose.ui.unit.dp
 import domain.Detail
 
-@ExperimentalComposeUiApi
 @Composable
 fun DetailComponent(
     index: Int,
